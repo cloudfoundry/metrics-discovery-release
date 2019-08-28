12f7081003054b446865c3250db3c27f98a9c522
Max Eshleman <meshleman@pivotal.io>
2019-08-28 15:55:42 -0600

    replace metrics_targets with prom_scraper_config
    [#168122112]

--- src/cmd/config-generator/main.go
+++ src/cmd/config-generator/main.go
@@ -33,10 +33,6 @@ func main() {
 	}
 
 	m := metrics.NewRegistry(logger,
-		metrics.WithDefaultTags(map[string]string{
-			"origin":    "loggregator.config_generator", //TODO
-			"source_id": "config_generator",
-		}),
 		metrics.WithTLSServer(config.MetricsPort, config.MetricsCertPath, config.MetricsKeyPath, config.MetricsCAPath),
 	)
 
