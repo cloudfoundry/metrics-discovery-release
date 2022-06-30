32c8f483e7fc6fcb5cbdf363e4d8939dcbb203a3
Carson Long <lcarson@vmware.com>
2022-06-30 09:16:30 -0600

    Upgrade go-loggregator v9

--- src/internal/collector/envelope.go
+++ src/internal/collector/envelope.go
@@ -8,7 +8,7 @@ import (
 	"sync"
 	"time"
 
-	"code.cloudfoundry.org/go-loggregator/v8/rpc/loggregator_v2"
+	"code.cloudfoundry.org/go-loggregator/v9/rpc/loggregator_v2"
 	metrics "code.cloudfoundry.org/go-metric-registry"
 	v2 "code.cloudfoundry.org/loggregator-agent-release/src/pkg/egress/v2"
 	"github.com/prometheus/client_golang/prometheus"
--- src/vendor/code.cloudfoundry.org/go-loggregator/v8/tools.go
+++
@@ -1,8 +0,0 @@
-//go:build tools
-// +build tools
-
-package loggregator
-
-import (
-	_ "github.com/onsi/ginkgo/ginkgo"
-)
